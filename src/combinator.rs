9d925ab14de841e8ebec994e13e564c5adc006d6
Andrej Kolčin <self@kaathewise.net>
2024-12-06 17:18:12 +0300

    [src] lib: implement transforms as `Parser` methods

--- src/combinator.rs
+++ src/combinator.rs
@@ -11,43 +11,13 @@ where
 	}
 }
 
-// Might be better as an implemnted method on the `Parser` trait.
-pub fn into<'a, I, O1, O2, E1, E2, P, Q>(
-	parser: P,
-) -> impl Parser<'a, I, O2, E2>
-where
-	I: 'a,
-	O1: Into<O2>,
-	E1: Into<E2>,
-	P: Parser<'a, I, O1, E1>,
-{
-	move |input: &'a I| match parser.parse(input) {
-		Ok((out, rest)) => Ok((out.into(), rest)),
-		Err(err) => Err(err.into()),
-	}
-}
-
-pub fn map_out<'a, I, O1, O2, E, P, F>(
-	parser: P,
-	f: F,
-) -> impl Parser<'a, I, O2, E>
-where
-	I: 'a,
-	P: Parser<'a, I, O1, E>,
-	F: Fn(O1) -> O2,
-{
-	move |input: &'a I| {
-		parser.parse(input).map(|(out, rest)| (f(out), rest))
-	}
-}
-
 pub fn value<'a, I, O, E, V, P>(value: V, parser: P) -> impl Parser<'a, I, V, E>
 where
 	I: 'a,
 	P: Parser<'a, I, O, E>,
 	V: Clone,
 {
-	map_out(parser, move |_| value.clone())
+	parser.map_out(move |_| value.clone())
 }
 
 /// Swaps the parser results: if the underlying parser succeeds, `not` will
--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,39 @@ pub type PResult<'a, I, O, E> = Result<(O, &'a I), E>;
 
 pub trait Parser<'a, I, O, E>
 where
-	I: ?Sized,
+	I: 'a + ?Sized,
 {
 	fn parse(&self, input: &'a I) -> PResult<'a, I, O, E>;
+
+	fn into<OX, EX>(self) -> impl Parser<'a, I, OX, EX>
+	where
+		Self: Sized,
+		O: Into<OX>,
+		E: Into<EX>,
+	{
+		move |input: &'a I| match self.parse(input) {
+			Ok((out, rest)) => Ok((out.into(), rest)),
+			Err(err) => Err(err.into()),
+		}
+	}
+
+	fn map_out<OX, F>(self, f: F) -> impl Parser<'a, I, OX, E>
+	where
+		Self: Sized,
+		F: Fn(O) -> OX,
+	{
+		move |input: &'a I| {
+			self.parse(input).map(|(out, rest)| (f(out), rest))
+		}
+	}
+
+	fn map_err<EX, F>(self, f: F) -> impl Parser<'a, I, O, EX>
+	where
+		Self: Sized,
+		F: Fn(E) -> EX,
+	{
+		move |input: &'a I| self.parse(input).map_err(|err| f(err))
+	}
 }
 
 impl<'a, I, O, E, F> Parser<'a, I, O, E> for F
