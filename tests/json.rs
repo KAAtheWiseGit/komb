cbba82205f83e32296a215dab8f6785c3f560ffa
Andrej Kolčin <self@kaathewise.net>
2024-12-23 16:33:00 +0300

    [tests] json: basic boolean and array parsing

--- tests/json.rs
+++ tests/json.rs
@@ -3,8 +3,13 @@
 use std::collections::HashMap;
 use std::convert::Infallible;
 
-use komb::{string, PResult, Parser};
+use komb::{
+	combinator::{choice, delimited, fold, option},
+	string::{char, literal, one_of0},
+	PResult, Parser,
+};
 
+#[derive(Clone)]
 enum Value {
 	Null,
 	Bool(bool),
@@ -14,12 +19,44 @@ enum Value {
 	Object(HashMap<String, Value>),
 }
 
-fn whitespace(input: &str) -> PResult<str, (), Infallible> {
-	string::one_of0(&[' ', '\n', '\r', '\t'])
-		.parse(input)
-		.map(|(_, rest)| ((), rest))
+fn whitespace(input: &str) -> PResult<str, (), ()> {
+	let out = one_of0(&[' ', '\n', '\r', '\t'])
+		.map_out(|_| ())
+		.map_err(|_| ())
+		.parse(input);
+	out
+}
+
+fn bool(input: &str) -> PResult<str, bool, ()> {
+	delimited(
+		whitespace,
+		choice((
+			literal("true").map_out(|_| true).map_err(|_| ()),
+			literal("false").map_out(|_| false).map_err(|_| ()),
+		)),
+		whitespace,
+	)
+	.parse(input)
+}
+
+fn array(input: &str) -> PResult<str, Vec<Value>, ()> {
+	let p = value.before(option(char(',')).map_err(|_| ()));
+	let folded = fold(p, Vec::new(), |acc, value| acc.push(value));
+	let delimited = delimited(
+		char('[').map_err(|_| ()),
+		folded,
+		char(']').map_err(|_| ()),
+	);
+
+	delimited.parse(input)
 }
 
 fn value(input: &str) -> PResult<str, Value, ()> {
 	todo!()
+}
+
+#[test]
+fn test_bool() {
+	let s = "   true  ";
+	assert_eq!(Ok((true, "")), bool(s));
 }
